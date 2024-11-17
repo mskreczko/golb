841534992a4caf7b38739e197f112b76f1e36e6d
Michał Skreczko <mic.skreczko@gmail.com>
2024-11-17 20:51:28 +0100

    refactor: export request and response copying functions to http_utils.go

---
+++ http_utils.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func copyRequest(r *http.Request, ctx context.Context, targetServer *Server) *http.Request {
+	req := r.Clone(ctx)
+	req.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+	req.URL = &url.URL{
+		Scheme: "http",
+		Host:   strings.Split(targetServer.addr, "//")[1],
+		Path:   url.QueryEscape(req.RequestURI),
+	}
+	req.RequestURI = ""
+	return req
+}
+
+func copyResponse(w http.ResponseWriter, resp *http.Response) {
+	w.WriteHeader(resp.StatusCode)
+	if resp.Body != nil {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write(body)
+	}
+}
--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"context"
-	"io"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 func main() {
@@ -49,27 +46,4 @@ func proxyHandler(w http.ResponseWriter, r *http.Request, ctx context.Context, h
 
 	defer resp.Body.Close()
 	copyResponse(w, resp)
-}
-
-func copyRequest(r *http.Request, ctx context.Context, targetServer *Server) *http.Request {
-	req := r.Clone(ctx)
-	req.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
-	req.URL = &url.URL{
-		Scheme: "http",
-		Host:   strings.Split(targetServer.addr, "//")[1],
-		Path:   url.QueryEscape(req.RequestURI),
-	}
-	req.RequestURI = ""
-	return req
-}
-
-func copyResponse(w http.ResponseWriter, resp *http.Response) {
-	w.WriteHeader(resp.StatusCode)
-	if resp.Body != nil {
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.Write(body)
-	}
 }
