b8f6e0c97c2cb5b1520a8e292e54f5ca17ee2ce1
Michał Skreczko <mic.skreczko@gmail.com>
2024-11-22 22:16:07 +0100

    feat: add time serializer for custom timestamp format

---
+++ lb/serializer.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type JSONTime time.Time
+
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("2006-01-02 15:04:05"))
+	return []byte(stamp), nil
+}
