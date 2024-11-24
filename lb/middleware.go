cf3f192a4f59861db371b41ffb27ac42a542e36d
Michał Skreczko <mic.skreczko@gmail.com>
2024-11-24 21:26:33 +0100

    feat: add middleware for logging incoming requests and cors handling

---
+++ lb/middleware.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func LogHttp(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Incoming request: %s %s %s", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func CORS(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/metrics/server" {
+			next.ServeHTTP(w, r)
+		}
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			http.Error(w, "No Content", http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
