9e8497cee2c3246b0b26b6b97df8171736fcff52
Michał Skreczko <mic.skreczko@gmail.com>
2024-12-01 18:28:25 +0100

    fix: fix middleware

--- lb/http_server.go
+++ lb/http_server.go
@@ -7,16 +7,15 @@ import (
 
 func NewServer(ctx context.Context, httpClient *http.Client, sp *ServerPool) http.Handler {
 	mux := http.NewServeMux()
+	var handler http.Handler = mux
+	handler = LogHttp(handler)
+	handler = CORS(handler)
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		proxyHandler(w, r, ctx, httpClient, sp)
 	})
 	mux.HandleFunc("/metrics/servers", func(w http.ResponseWriter, r *http.Request) {
 		sp.GetAllServers(w)
 	})
-
-	var handler http.Handler = mux
-	handler = LogHttp(handler)
-	handler = CORS(handler)
 
 	return handler
 }
